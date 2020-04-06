c486764b36c54752bb98ee21112bf7e11cd2ff83
christides11 <christides11@hotmail.com>
2020-04-05 20:02:27 -0700

    Start of song support.

--- Assets/_TAPI/Modding/ModDefinition.cs
+++ Assets/_TAPI/Modding/ModDefinition.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using TAPI.Core;
 using TAPI.Entities.Shared;
 using TAPI.GameMode;
+using TAPI.Sound;
 using UnityEngine;
 
 namespace TAPI.Modding
@@ -17,6 +18,7 @@ namespace TAPI.Modding
         [SerializeField] private List<GameModeDefinition> gamemodes = new List<GameModeDefinition>();
         [SerializeField] private List<StageDefinition> stages = new List<StageDefinition>();
         [SerializeField] private List<StageCollection> stageCollections = new List<StageCollection>();
+        [SerializeField] private List<SongDefinition> songs = new List<SongDefinition>();
 
         /// <summary>
         /// Gets all the characters in the mod.
@@ -59,7 +61,7 @@ namespace TAPI.Modding
         }
 
         /// <summary>
-        /// Gets all the scenes in the mod.
+        /// Gets all the stages in the mod.
         /// </summary>
         /// <returns>A list of all the scenes in the mod.</returns>
         public virtual List<StageDefinition> GetStageDefinitions()
@@ -68,9 +70,9 @@ namespace TAPI.Modding
         }
 
         /// <summary>
-        /// Gets a scene by name.
+        /// Gets a stage by name.
         /// </summary>
-        /// <param name="sceneName"></param>
+        /// <param name="stageName">The name of the stage.</param>
         /// <returns>The SceneDefinition with the scene name given. If it does not exist,
         /// returns null.</returns>
         public virtual StageDefinition GetStageDefinition(string stageName)
@@ -78,23 +80,44 @@ namespace TAPI.Modding
             return stages.FirstOrDefault(x => x.stageName.ToLower() == stageName.ToLower());
         }
 
+        /// <summary>
+        /// Gets all the stage collections in this mod.
+        /// </summary>
+        /// <returns>A list of all the stage collections in the mod.</returns>
         public virtual List<StageCollection> GetStageCollections()
         {
             return stageCollections;
         }
 
-        public virtual StageCollection GetStageCollection(string stageCollection)
+        /// <summary>
+        /// Gets a stage collection by name.
+        /// </summary>
+        /// <param name="stageCollectionName">The name of the stage collection.</param>
+        /// <returns>The StageCollection with the name given. If it does not exist,
+        /// returns null.</returns>
+        public virtual StageCollection GetStageCollection(string stageCollectionName)
         {
-            return stageCollections.FirstOrDefault(x => x.collectionName.ToLower() == stageCollection.ToLower());
+            return stageCollections.FirstOrDefault(x => x.collectionName.ToLower() == stageCollectionName.ToLower());
         }
 
         /// <summary>
-        /// Loads the scene requested. If a mod, just return false.
+        /// Gets all the songs in this mod.
         /// </summary>
-        /// <returns>False if this is a umod mod.</returns>
-        public virtual async Task<bool> LoadScene(string sceneName)
+        /// <returns>A list of all the songs in the mod.</returns>
+        public virtual List<SongDefinition> GetSongDefinitions()
         {
-            return false;
+            return songs;
+        }
+
+        /// <summary>
+        /// Gets a ngso by name.
+        /// </summary>
+        /// <param name="songName">The name of the song.</param>
+        /// <returns>The song with the name given. If it does not exist,
+        /// returns null.</returns>
+        public virtual SongDefinition GetSongDefinition(string songName)
+        {
+            return songs.FirstOrDefault(x => x.songName.ToLower() == songName.ToLower());
         }
     }
 }
